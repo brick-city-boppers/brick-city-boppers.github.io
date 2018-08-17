ec7e335cd8466e53106847367ca9e40af10ec244
lance13c <pianoman13c@gmail.com>
2018-08-17 02:16:53 -0400

    researched and added fonts

--- bcb-website/src/main.js
+++ bcb-website/src/main.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill'
-import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/solid.css'
+import '@fortawesome/fontawesome-free/css/brands.css'
 import Vue from 'vue'
 import {
   Vuetify, // required
@@ -11,9 +12,9 @@ import {
   transitions,
   VList,
   VIcon,
-  VFooter
+  VFooter,
+  VParallax
  } from 'vuetify'
-import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue'
 import router from './router'
 import * as VueGoogleMaps from "vue2-google-maps";
@@ -22,6 +23,8 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+
+import './stylus/main.styl';
 
 // Replace this with your project's endpoint
 const GRAPHCMS_API = 'https://api-useast.graphcms.com/v1/cjkvr0bk4024i01bnlo1xs2vc/master'
@@ -50,7 +53,8 @@ Vue.use(Vuetify, {
     transitions,
     VList,
     VIcon,
-    VFooter
+    VFooter,
+    VParallax
   },
 })
 
--- bcb-website/src/views/Home.vue
+++ bcb-website/src/views/Home.vue
@@ -6,19 +6,23 @@
       
         <v-layout column class="bcb-section">
           <!-- <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5"> -->
-          <v-flex tag="h1" class="text-xs-center bcb-home__title bcb-top">Brick City Boppers</v-flex>
-          <v-flex tag="h4" class="text-xs-center ">RIT's finest swing dance club. Come learn the styles of Lindy Hop, Charleston, and many more</v-flex>
+          <section class="bcb-heading">
+            <v-flex tag="h1" class="text-xs-center display-3 font-weight-medium bcb-home__title bcb-top">Brick City Boppers</v-flex>
+            <v-flex tag="h4" class="text-xs-center subheading font-weight-thin font-italic bcb-home__sub-title">RIT's finest swing dance club. Come learn the styles of Lindy Hop, Charleston, and many more</v-flex>
+          </section>
           
-          <v-flex tag="div" class="bcb-spacer"></v-flex>
+          <!-- <v-parallax class="bcb-hero" src="heroImage"></v-parallax> -->
+          
+          <v-flex tag="div" class="bcb-spacer--large"></v-flex>
+
           <section id="weekly-meeting">
-            <v-flex href="#weekly-meeting" tag="h2" class="">Weekly Meeting</v-flex>
-            
-              <v-expansion-panel popout>
+            <v-flex href="#weekly-meeting" tag="h2" class="headline font-weight-medium bcb-meeting__header uppercase mb-1 ">Weekly Meeting</v-flex>
+              <v-expansion-panel popout class="bcb-meeting__detials">
                 <v-expansion-panel-content>
                   <div slot="header">
                     <v-layout row wrap>
-                      <v-flex xs2 tag="h4" class="mt-1 mb-0">Location</v-flex>
-                      <v-flex xs10 tag="p" class="mt-1 mb-0">Campus Center 2061</v-flex>
+                      <v-flex xs4 tag="h4" class="mt-1 mb-0">Location</v-flex>
+                      <v-flex xs8 tag="p" class="mt-1 mb-0">Campus Center 2061</v-flex>
                     </v-layout>
                   </div>
                   <v-card>
@@ -28,8 +32,8 @@
                   <v-expansion-panel-content readonly class="bcb-hide-icon">
                   <div slot="header">
                     <v-layout row wrap>
-                      <v-flex xs2 tag="h4">Time</v-flex>
-                      <v-flex xs10 tag="p">8:00pm EST</v-flex>
+                      <v-flex xs4 tag="h4">Time</v-flex>
+                      <v-flex xs8 tag="p">8:00pm EST</v-flex>
                     </v-layout>
                   </div>
                   <v-card>
@@ -53,6 +57,7 @@
 <script>
 
 import GoogleMap from "@/components/GoogleMap";
+import heroImage from "@/assets/showcase/bcb(1).jpg";
 
 export default {
   name: 'Home',
@@ -61,17 +66,68 @@ export default {
   },
   data () {
     return {
-      temp: 'test'
+      temp: 'test',
+      heroImage: heroImage
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style lang='scss'>
+
+.application--wrap {
+  /* background: -moz-radial-gradient(center, ellipse cover, #8C5EFF 0%, #000000 100%); /* ff3.6+ */
+  /* background: -webkit-radial-gradient(center, ellipse cover, #8C5EFF 0%, #000000 100%); safari5.1+,chrome10+ */
+  /* background: -o-radial-gradient(center, ellipse cover, #8C5EFF 0%, #000000 100%); opera 11.10+ */
+  /* background: radial-gradient(ellipse at center, #8C5EFF 0%, #000000 100%); w3c */
+
+  /* background: radial-gradient(ellipse at bottom, rgba(168,169,224,1) 0%,rgba(33,19,73,1) 100%); W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  background: linear-gradient(180deg, #5900BF 0%, #5900BF 1%, #EDE1F0 100%); /* w3c */
+}
 
 .bcb-top {
   margin-top: 70px;
+}
+.bcb-heading {
+  position: relative;
+  z-index: 10;
+  color: #FFFEFE;
+}
+
+/* .bcb-home__title {
+
+}
+
+.bcb-home__sub-title {
+
+} */
+
+.bcb-hero {
+  position: absolute;
+  z-index: 0;
+  max-height: 600px;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+
+#weekly-meeting {
+  z-index: 10;
+}
+
+.application .bcb-meeting__header {
+  margin-left: 2.4%;
+  font-family: "Baloo Bhai" !important;
+}
+
+/* .bcb-meeting__detials {
+  display: flex;
+  justify-content: left;
+} */
+
+.uppercase {
+  text-transform: uppercase;
 }
 
 .bcb-container {
@@ -82,8 +138,12 @@ export default {
     visibility: hidden;
 }
 
-.bcb-spacer {
+.bcb-spacer--med {
   height: 40px;
+}
+
+.bcb-spacer--large {
+  height: 100px;
 }
 
 .bcb-section {
